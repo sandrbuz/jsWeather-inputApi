1be134f17c0b70fab97fad812005090e1f83342d
sandrbuz <sandrbuz@mail.ru>
2021-04-02 14:34:49 +0300

    redesign

--- script.js
+++ script.js
@@ -4,28 +4,32 @@ let loader = document.querySelector('#loader');
 
 const getWeather = (cityName) => {
     if (cityName) {
-        loader.style.color = 'white';
+        loader.style.color = 'black';
         loader.innerText = 'loading...';
         loader.style.opacity = '1';
         btnGet.disabled = true;
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=655fdcf1aed1280abf8e870e95b28149`)
             .then(resp => resp.json())
             .then(data => {
-                // console.log(data);
-                // loader.innerText = '';
+                console.log(data);
                 loader.style.opacity = '0';
                 btnGet.disabled = false;
 
+
                 document.querySelector('.city').textContent = data.name;
+                document.querySelector('.now').textContent = 'now';
                 document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
                 document.querySelector('.weather').textContent = data.weather[0]['description'];
-                // document.querySelector('.icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
                 let val = `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`;
                 $('#icon').attr('src', val);
             })
             .catch(function () {
-                console.log('Ошибка 404')
-                document.querySelector('#loader').textContent = 'error';
+                document.querySelector('.city').textContent = '________';
+                document.querySelector('.temp').textContent = '';
+                document.querySelector('.weather').textContent = '';
+                document.querySelector('.now').textContent = '';
+                $('#icon').attr('src', '');
+                document.querySelector('#loader').textContent = 'City name error';
                 loader.style.opacity = '1';
                 loader.style.color = 'red';
                 loader.style.fontSize = '30px';
@@ -37,9 +41,7 @@ const getWeather = (cityName) => {
         loader.style.fontSize = '30px';
         loader.style.opacity = '1';
         loader.innerText = 'Please enter city';
-
     }
-
 }
 
 
@@ -52,15 +54,19 @@ getWeather('Moscow');
 
 
 let userInput = document.querySelector('.inp');
-
 btnGet.addEventListener('click', function () {
-
-
     const cityName = userInput.value;
-
     getWeather(cityName)
 
 })
+
+
+
+
+
+
+
+
 
 
 
